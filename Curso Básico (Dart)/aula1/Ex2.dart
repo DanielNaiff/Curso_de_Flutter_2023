893f423bc43586fd95c8a5969b3b7c01e6c95a90
danielnaiff <daniel.naiff@yahoo.com.br>
2023-07-25 16:39:39 -0300

    Delete Ex2.dart

--- Curso Básico (Dart)/aula1/Ex2.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  String redacao = 'As minhas férias foi a mais melhor do mundo';
-  print('Corrigindo "$redacao" para "${redacao.replaceAll('mais melhor','melhor')}"');
-  redacao = redacao.toUpperCase();
-  print('Tem a palavra férias?: ${redacao.contains('FÉRIAS')}');
-  redacao = redacao.replaceAll(' ', '');
-  print('Quantidade de palavras: ${redacao.length}');
-}
