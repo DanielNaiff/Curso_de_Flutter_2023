e7d0cc96675014bed7a4ec17510430995212da0b
DanielNaiff <68018516+DanielNaiff@users.noreply.github.com>
2023-07-26 10:48:21 -0300

    Update ex03.dart

--- Curso Básico (Dart)/aula3/ex03.dart
+++ Curso Básico (Dart)/aula3/ex03.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 void main(){
-  List <int> lista = [0,1,2,3,4,5,6,67,7,8,9,34,3,432,23,42,1,4,454,5];
-  List <int> listapar = [];
+  List lista = [0,1,2,3,4,5,6,67,7,8,9,34,3,432,23,42,1,4,454,5];
+  List listapar = [];
   print('Digite o preço máximo:');
   int max = int.parse(stdin.readLineSync()!);
-  for (int i = 0; i <= lista.length; i++){
-    if ((lista[i] <= max) && (lista[i]%2==0)){
+  for (int i = 0; i < lista.length; i++){
+    if (lista[i] <= max && lista[i]%2==0){
       listapar.add(lista[i]);
     }
   }
