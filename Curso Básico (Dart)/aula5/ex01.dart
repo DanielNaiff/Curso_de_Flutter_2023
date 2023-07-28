4852464110e4a3387c43dc85d3b6a9102eb2ae94
Daniel Naiff <68018516+DanielNaiff@users.noreply.github.com>
2023-07-28 11:59:38 -0300

    Update ex01.dart

--- Curso Básico (Dart)/aula5/ex01.dart
+++ Curso Básico (Dart)/aula5/ex01.dart
@@ -1,19 +1,13 @@
 import 'dart:async';
 
 Future <String> fetchData() async {
-    // await Future.delayed(Duration(seconds: 3)).then((_){
-    //   print('Passaram-se 3 segundos');
-    // });
-    // return 'Dados obtidos';
-
     await Future.delayed(Duration(seconds: 3));
   return "Dados obtidos";
 }
 Future<void> main() async {
 
-   fetchData().then((fetchData){
-    print(fetchData);
-  });
-  // print(result);
-  
+   fetchData().then((fetchData){print(fetchData);});
+
+  print('Aguarde 3 segundos');
 }
+
