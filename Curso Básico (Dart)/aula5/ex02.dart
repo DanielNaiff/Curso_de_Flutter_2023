74d4897b9a0362be3fa3c0c5ec97e0eaed6046ce
Daniel Naiff <68018516+DanielNaiff@users.noreply.github.com>
2023-07-28 17:33:34 -0300

    Update ex02.dart

--- Curso Básico (Dart)/aula5/ex02.dart
+++ Curso Básico (Dart)/aula5/ex02.dart
@@ -1,18 +1,17 @@
 import 'dart:async';
 
 Stream<int> contdownnumber(int max) async*{
-  for (int i = max; i == 1 ; i--){
-    await Future.delayed(const Duration(seconds: 1));
+  for (int i = max; i >= 1 ; i--){
+    print('Aguarde 1 segundo');
     yield i;
+    await Future.delayed(const Duration(seconds: 1));
   }
 }
 Future <void> main() async {
 
-   final stream<int> numberStream = contdownnumber(5);
+   final Stream<int> numberStream = contdownnumber(5);
 
   numberStream.listen((number){
     print(number);
   });
-
-  print('Aguarde 1 segundos');
 }
