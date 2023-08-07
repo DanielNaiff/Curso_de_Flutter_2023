2fc504cf4fe11b8deb4a3683683ae23e20e7f365
danielnaiff <daniel.naiff@yahoo.com.br>
2023-08-06 21:35:10 -0300

    Tic_Tac_Toe

---
+++ Jogo_da_velha/lib/lógica.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:calculadora_flutter/lógica.dart';
+import 'package:calculadora_flutter/screens.dart';
+import 'package:calculadora_flutter/main.dart';
+
+String jogadorAtual = 'X';
+String jogadordaVez = '';
+String vencedor = '';
+bool ganhador = false;
+int jogadas = 0;
+int placarjogador1 = 0;
+int placarjogador2 = 0;
+
+void interacao({required linha, required coluna, required matriz}) {
+  matriz[linha][coluna] = jogadorAtual;
+
+  if (jogadorAtual == 'X') {
+    jogadordaVez = 'jogador2';
+    jogadorAtual = 'O';
+  } else {
+    jogadordaVez = 'jogador1';
+    jogadorAtual = 'X';
+  }
+
+  for (var l = 0; l < 3; l++) {
+    if (matriz[l][0] == matriz[l][1] &&
+        matriz[l][1] == matriz[l][2] &&
+        matriz[l][0] != '') {
+      vencedor = jogadordaVez;
+      ganhador = true;
+    }
+  }
+
+  for (var c = 0; c < 3; c++) {
+    if (matriz[0][c] == matriz[1][c] &&
+        matriz[1][c] == matriz[2][c] &&
+        matriz[0][c] != '') {
+      vencedor = jogadordaVez;
+      ganhador = true;
+    }
+  }
+
+  if (matriz[0][0] == matriz[1][1] &&
+      matriz[1][1] == matriz[2][2] &&
+      matriz[0][0] != '') {
+    vencedor = jogadordaVez;
+    ganhador = true;
+  }
+
+  if (matriz[2][0] == matriz[1][1] &&
+      matriz[1][1] == matriz[0][2] &&
+      matriz[2][0] != '') {
+    vencedor = jogadordaVez;
+    ganhador = true;
+  }
+
+  jogadas++;
+}
---
+++ Jogo_da_velha/lib/screens.dart
@@ -0,0 +1,25 @@
+import 'package:calculadora_flutter/l%C3%B3gica.dart';
+import 'package:flutter/material.dart';
+//import 'package:calculadora_flutter/main.dart';
+import 'package:calculadora_flutter/classes.dart';
+
+class Tela2 extends StatelessWidget {
+  final String jogador1;
+  final String jogador2;
+  const Tela2({
+    super.key,
+    required this.jogador1,
+    required this.jogador2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Coluna(
+        jogador1: jogador1,
+        jogador2: jogador2,
+      ),
+    ));
+  }
+}
